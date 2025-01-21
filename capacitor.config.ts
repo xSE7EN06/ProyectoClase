9cc616d147268376470bb884664400ae461b7ef8
xSE7EN06 <agao6105@gmail.com>
2025-01-20 22:40:25 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'hola_mundo',
+  webDir: 'www'
+};
+
+export default config;
